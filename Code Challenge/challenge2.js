9aaf4d502cbe178f1479906613843ad0c5c9c351
JustinHuy <trannhathuy1305@gmail.com>
2022-04-30 08:53:55 +0700

    l

--- Code Challenge/challenge2.js
+++ Code Challenge/challenge2.js
@@ -6,5 +6,8 @@ let total = [];
 
 function calcTip(bill) {
     let tip = (50 < bill < 300) ? bill * 0.15 : bill * 0.2;
+    bills.push(bill);
+    tips.push(tip);
+    total.push(tip + bill);
     return tip;
 }
